e20d577da0d278facf0a3dbdd718bf7d79944a90
Matteo <teo_exc@hotmail.it>
2019-11-12 10:56:16 +0100

    - Conversion from 1 to 10 in I to X

--- test/source/dssc/roman/RomanNumeral.java
+++ test/source/dssc/roman/RomanNumeral.java
@@ -3,6 +3,8 @@ package dssc.roman;
 public class RomanNumeral {
 
     private int decimal;
+    private String Unit[]={"","I","II","III","IV","V","VI","VII","VIII","IX","X"};
+   // private String Decimal[]={};
 
     public RomanNumeral(int decimal) {
         this.decimal = decimal;
@@ -10,10 +12,30 @@ public class RomanNumeral {
 
     @Override
     public String toString() {
-        if (this.decimal == 0) {
+        if(this.decimal<=10){
+            return Unit[this.decimal];
+        }else{
+            return Unit[1];
+        }
+
+        /*switch(this.decimal) {
+            case 1:
+            case 2:
+            case 3:
+                return "I".repeat(decimal);
+            case 5:
+                return "V";
+            case 10:
+                return "X";
+
+            default:
+                return "";
+        }*/
+
+        /*if (this.decimal == 0) {
             return "";
         } else {
             return "I".repeat(decimal);
-        }
+        }*/
     }
 }
